b74c585bce117c9e8d02fa1767e111f0593a2785
david.yin <david_3g@sina.cn>
2017-12-17 10:49:40 +0800

    simpleapi模版代码规范微调

--- templates/project/simpleapi/index.php
+++ templates/project/simpleapi/index.php
@@ -8,8 +8,6 @@ define('APP_PATH', BASE_PATH . '/app');
 define('LOG_PATH', BASE_PATH . '/logs');
 
 include(BASE_PATH . '/vendor/autoload.php');
-
-// 导入.env环境变量
 $dotenv = new Dotenv\Dotenv(BASE_PATH);
 $dotenv->load();
 
