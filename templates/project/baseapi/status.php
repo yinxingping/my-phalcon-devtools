c939e0cbf42ede09746a227438dfe8aba924dad0
david.yin <david_3g@sina.cn>
2018-01-03 23:26:58 +0800

    baseapi增加参数验证错误类型

--- templates/project/baseapi/status.php
+++ templates/project/baseapi/status.php
@@ -2,6 +2,7 @@
 define('OK', 0);
 define('DB_ERROR', 1);
 define('EXCEPTION', 2);
+define('VALID_ERROR', 3);
 
 define('STATUS',[
     'ok' => [
@@ -13,10 +14,12 @@ define('STATUS',[
     'exception' => [
         'code' => EXCEPTION,
     ],
+    'valid_error' => [
+        'code' => VALID_ERROR,
+    ],
     'record_not_exists' => [
         'code' => DB_ERROR,
         'message' => '要操作的记录不存在',
-        'type' => 'record_not_exists',
     ],
 ]);
 
